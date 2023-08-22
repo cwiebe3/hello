12527a387eb0eb4317cc50117a96e9c7b8222bb9
Carston Wiebe <cwiebe3@husker.unl.edu>
2023-08-22 11:21:23 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * author: Carston Wiebe (cwiebe3@husker.unl.edu)
+ * date: 2023/08/22
+ *
+ * Hello world program for hack 1
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Carston Wiebe\n");
+
+  return 0;
+}
